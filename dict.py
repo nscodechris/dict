e2c5b8e058ad2c8300fbe76a0b60bdb08b37c614
nscodechris <christoffer.vdmab@gmail.com>
2022-06-09 11:42:33 +0200

    edit cmd

--- dict.py
+++ dict.py
@@ -34,7 +34,8 @@ while True: ## REPL - Read Execute Program Loop
     print("Commands: \n--list\n--add\n--delete\n--quit!")
     cmd = input("Command: ")
     if cmd == "list":
-        print(read_dict(conn))
+        for i, wd, trans in read_dict(conn):
+            print(f"{i}: {wd} - {trans}")
     elif cmd == "add":
         name = input("  Word: ")
         phone = input("  Translation: ")
